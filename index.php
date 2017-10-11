ff17ef1b8b24e62bea94f681e5da364600ef603f
Fabrice Gabriel Calixte <f.gabrielcalixte@gmail.com>
2017-10-11 14:28:54 +0200

    creation du fichier footer.php

--- index.php
+++ index.php
@@ -74,6 +74,9 @@ require 'header.php';
   </div>
 </section>
 <footer>
+  <?php
+      include 'footer.php';
+  ?>    
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
