c5ebb84b17a00a512e3124616845f25a1bb50598
Fabrice Gabriel Calixte <f.gabrielcalixte@gmail.com>
2017-10-12 16:14:07 +0200

    test couleur footer

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <div class="container-fluid footer">
     <div class="row">
         <ul class="col-md-offset-1 col-md-3 footer_list ">
-          <li><a href="#">Accueil</a></li>
-          <li><a href="#">Cartes</a></li>
-          <li><a href="#">Lieux</a></li>
-        </ul>
+          <li class="footer-accueil"><a href="#">Accueil</a></li>
+          <li class="footer-cartes"><a href="#">Cartes</a></li>
+          <li class="footer-lieux"><a href="#">Lieux</a></li>
+        </ul> 
         <ul class="col-md-3 footer_list">
           <li>Tel: 06-51-75-79-20</li>
           <li>Mail: calunapizz@gmail.com</li>
