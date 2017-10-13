5bf775dc97ebf5f5540de0b867cc519ac7262be1
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2017-10-13 10:33:59 +0200

    passage mvc

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+use Cataluna\Controller\HomeController;
+use Cataluna\Controller\MenuController;
+
+require '../vendor/autoload.php';
+require '../connect.php';
+
+// ROUTER
+if (!empty($_GET['route'])) {
+    if ($_GET['route'] === 'carte') {
+        // charge la page de la carte
+        $controller = new MenuController();
+        echo $controller->showAction();
+
+    } elseif ($_GET['route'] === 'lieux') {
+        // charge page des lieux
+        $controller = new LocationController();
+        echo $controller->showAction();
+    }
+} else {
+    // charge homepage
+    $controller = new HomeController();
+    echo $controller->showAction();
+}
+
+?>
---
+++ src/Controller/LocationController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 13/10/17
+ * Time: 09:38
+ */
+
+namespace Cataluna\Controller;
+
+
+class LocationController
+{
+
+}
