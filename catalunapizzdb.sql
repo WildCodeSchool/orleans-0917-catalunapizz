623fdd71ae9e613665ae7dec10267257ae8ee67d
Fabrice Gabriel Calixte <f.gabrielcalixte@gmail.com>
2017-10-18 11:37:11 +0200

    modification base de donnée

--- catalunapizzdb.sql
+++ catalunapizzdb.sql
@@ -32,8 +32,10 @@ DROP TABLE IF EXISTS `about_us`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `about_us` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `about-us` text NOT NULL,
+  `about-us` text,
+  `news_title` varchar(45) DEFAULT NULL,
   `news` text,
+  `news_picture` text,
   `mail` varchar(100) DEFAULT NULL,
   `tel` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -46,7 +48,7 @@ CREATE TABLE `about_us` (
 
 LOCK TABLES `about_us` WRITE;
 /*!40000 ALTER TABLE `about_us` DISABLE KEYS */;
-INSERT INTO `about_us` VALUES (1,'',NULL,'catalunapizz@gmail.com','06-51-75-79-20');
+INSERT INTO `about_us` VALUES (1,'',NULL,NULL,NULL,'catalunapizz@gmail.com','06-51-75-79-20');
 /*!40000 ALTER TABLE `about_us` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,7 +64,7 @@ CREATE TABLE `category` (
   `name` varchar(45) DEFAULT NULL,
   `picture` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +73,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'BASE TOMATE',NULL),(2,'BASE CREME',NULL),(3,'DESSERT',NULL);
+INSERT INTO `category` VALUES (1,'BASE TOMATE',NULL),(2,'BASE CREME',NULL),(3,'DESSERT',NULL),(4,'BOCADILLOS',NULL);
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,7 +145,7 @@ CREATE TABLE `pizza` (
   PRIMARY KEY (`id`),
   KEY `fk_pizza_category1_idx` (`category_id`),
   CONSTRAINT `fk_pizza_category1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -152,7 +154,7 @@ CREATE TABLE `pizza` (
 
 LOCK TABLES `pizza` WRITE;
 /*!40000 ALTER TABLE `pizza` DISABLE KEYS */;
-INSERT INTO `pizza` VALUES (1,'La Margherita','Sauce tomate, fromage, olives',5.20,7.00,1),(2,'La Jamòn','Sauce tomate, jambon blanc, fromage, olives',6.50,8.20,1),(3,'La Regina','Sauce tomate, jambon blanc, champignons, fromage',7.00,9.00,1),(4,'La Catalane','Sauce tomate, chorizo, oignons rouges, poivrons, fromage',7.50,10.00,1),(5,'L\'orientale','Sauce tomate, merguez, oignons rouges, poivrons, fromage',7.50,10.00,1),(6,'La Cabra','Sauce tomate, chèvre, fromage, (accompagnement au choix : miel, persillade, ciboulette)',7.80,10.50,1),(7,'La Calzone','Sauce tomate, jambon blanc, oeuf, fromage',7.80,10.50,1),(8,'La 4 Fromages','Sauce tomate, bleu, parmesan, emmental, mozzarella ',9.00,11.50,1),(9,'L\'Hawaïenne','Sauce tomate, jambon blanc, ananas, crème fraîche',7.60,10.20,1),(10,'La Temporadas','Sauce tomate, jambon blanc, champignons, artichauts, poivrons, oeuf, fromage',9.00,11.50,1),(11,'La Vegetariana','Sauce tomate, tomate fraîche, champignons, poivrons, oignons rouges, artichauts, fromage, olives',8.00,10.50,1),(12,'La Barcelona','Sauce tomate, boeuf haché, chèvre, champignons, oeuf, oignons rouges, fromage',9.00,12.00,1),(13,'La Malaga','Sauce tomate, boeuf haché, chèvre, oeuf, oignons rouges, persillade, fromage',10.10,13.50,1),(14,'La Mexicaine','Sauce tomate, boeuf haché, chorizo, oignons rouges, persillade, oeuf, fromage',10.10,13.50,1),(15,'La Burger','Sauce tomate, boeuf haché, oignons rouges, cheddar, tomate fraîche, sauce burger, fromage',10.10,13.50,1),(16,'L\'Andalouse','Sauce tomate, chorizo, merguez, lardons, boeuf haché, jambon blanc, oeuf, piment d\'Espelette, fromage',11.00,14.00,1),(17,'La Flamenco','Sauce tomate, pomme de terre, Jambon Serrano, oeuf, piment d\'Espelette, fromage',8.50,11.00,1),(18,'La Caliente','Sauce tomate, jambon Serrano, chorizo, oeuf, piment d\'Espelette, fromage',8.00,12.00,1),(19,'La Campesina','Crème, lardons, oignons rouges, persillade, fromage',7.50,10.00,2),(20,'La Coklina','Crème, poulet, chèvre, miel, fromage',9.30,12.50,2),(21,'La Rambla','Crème, poulet, champignons, oeuf, oignons rouges, ciboulette, fromage',9.00,12.00,2),(22,'La Madrilène','Crème, jambon blanc, pomme de terre, oignons rouges, oeuf, ciboulette, fromage',9.00,12.00,2),(23,'La Del Mar','Crème, saumon fumé, ciboulette, fromage',10.10,13.50,2),(24,'La Carbonara','Crème, lardons, parmesan, oeuf, ciboulette, fromage',8.20,11.00,2),(25,'La savoyarde','Crème, lardons, pomme de terre, reblochon, fromage',9.70,13.00,2),(26,'La Boursin','Crème, boeuf haché, pomme de terre, Boursin, persillade, fromage',10.10,13.50,2),(27,'La Tonno','Crème, thon, oignons rouges, fromage, olives',9.00,12.00,2),(28,'La Sweety Poire','Crème, poire, amandes, coulis de chocolat',6.70,9.00,3),(29,'La Sweety Ananas','Crème, ananas, amandes, coulis de chocolat',6.70,9.00,3),(30,'La Smarties','Crème, Smarties, coulis de chocolat',7.50,10.00,3),(31,'La Tagada','Crème, bonbons Fraises Tagada, coulis de chocolat',7.50,10.00,3);
+INSERT INTO `pizza` VALUES (1,'La Margherita','Sauce tomate, fromage, olives',5.20,7.00,1),(2,'La Jamòn','Sauce tomate, jambon blanc, fromage, olives',6.50,8.20,1),(3,'La Regina','Sauce tomate, jambon blanc, champignons, fromage',7.00,9.00,1),(4,'La Catalane','Sauce tomate, chorizo, oignons rouges, poivrons, fromage',7.50,10.00,1),(5,'L\'orientale','Sauce tomate, merguez, oignons rouges, poivrons, fromage',7.50,10.00,1),(6,'La Cabra','Sauce tomate, chèvre, fromage, (accompagnement au choix : miel, persillade, ciboulette)',7.80,10.50,1),(7,'La Calzone','Sauce tomate, jambon blanc, oeuf, fromage',7.80,10.50,1),(8,'La 4 Fromages','Sauce tomate, bleu, parmesan, emmental, mozzarella ',9.00,11.50,1),(9,'L\'Hawaïenne','Sauce tomate, jambon blanc, ananas, crème fraîche',7.60,10.20,1),(10,'La Temporadas','Sauce tomate, jambon blanc, champignons, artichauts, poivrons, oeuf, fromage',9.00,11.50,1),(11,'La Vegetariana','Sauce tomate, tomate fraîche, champignons, poivrons, oignons rouges, artichauts, fromage, olives',8.00,10.50,1),(12,'La Barcelona','Sauce tomate, boeuf haché, chèvre, champignons, oeuf, oignons rouges, fromage',9.00,12.00,1),(13,'La Malaga','Sauce tomate, boeuf haché, chèvre, oeuf, oignons rouges, persillade, fromage',10.10,13.50,1),(14,'La Mexicaine','Sauce tomate, boeuf haché, chorizo, oignons rouges, persillade, oeuf, fromage',10.10,13.50,1),(15,'La Burger','Sauce tomate, boeuf haché, oignons rouges, cheddar, tomate fraîche, sauce burger, fromage',10.10,13.50,1),(16,'L\'Andalouse','Sauce tomate, chorizo, merguez, lardons, boeuf haché, jambon blanc, oeuf, piment d\'Espelette, fromage',11.00,14.00,1),(17,'La Flamenco','Sauce tomate, pomme de terre, Jambon Serrano, oeuf, piment d\'Espelette, fromage',8.50,11.00,1),(18,'La Caliente','Sauce tomate, jambon Serrano, chorizo, oeuf, piment d\'Espelette, fromage',8.00,12.00,1),(19,'La Campesina','Crème, lardons, oignons rouges, persillade, fromage',7.50,10.00,2),(20,'La Coklina','Crème, poulet, chèvre, miel, fromage',9.30,12.50,2),(21,'La Rambla','Crème, poulet, champignons, oeuf, oignons rouges, ciboulette, fromage',9.00,12.00,2),(22,'La Madrilène','Crème, jambon blanc, pomme de terre, oignons rouges, oeuf, ciboulette, fromage',9.00,12.00,2),(23,'La Del Mar','Crème, saumon fumé, ciboulette, fromage',10.10,13.50,2),(24,'La Carbonara','Crème, lardons, parmesan, oeuf, ciboulette, fromage',8.20,11.00,2),(25,'La savoyarde','Crème, lardons, pomme de terre, reblochon, fromage',9.70,13.00,2),(26,'La Boursin','Crème, boeuf haché, pomme de terre, Boursin, persillade, fromage',10.10,13.50,2),(27,'La Tonno','Crème, thon, oignons rouges, fromage, olives',9.00,12.00,2),(28,'La Sweety Poire','Crème, poire, amandes, coulis de chocolat',6.70,9.00,3),(29,'La Sweety Ananas','Crème, ananas, amandes, coulis de chocolat',6.70,9.00,3),(30,'La Smarties','Crème, Smarties, coulis de chocolat',7.50,10.00,3),(31,'La Tagada','Crème, bonbons Fraises Tagada, coulis de chocolat',7.50,10.00,3),(40,'Le Bocadillo Chorizo','Sauce tomate, emmental/mozzarella, chorizo',5.00,6.00,4),(41,'Le Bocadillo Merguez','Sauce tomate, emmental/mozzarella, merguez',5.00,6.00,4),(42,'Le Bocadillo Jambon','Sauce tomate, emmental/mozzarella, jambon',5.00,6.00,4),(43,'Le Bocadillo Chèvre','Sauce tomate, emmental/mozzarella, chèvre',5.00,6.00,4),(44,'Le Bocadillo 4 Fromages','Sauce tomate, emmental/mozzarella, 4 fromages',5.00,6.00,4),(45,'Le Bocadillo Lardons','Sauce tomate, emmental/mozzarella, lardons',5.00,6.00,4),(46,'Le Bocadillo Viande Hachée','Sauce tomate, emmental/mozzarella, viande hachée',5.00,6.00,4),(47,'Le Bocadillo Catalan','Sauce tomate, emmental/mozzarella, champignon, jambon Serrano',5.00,6.00,4);
 /*!40000 ALTER TABLE `pizza` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,4 +219,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-16 14:21:21
+-- Dump completed on 2017-10-18 11:34:58
