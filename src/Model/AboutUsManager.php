54cf036691fa4c5161e405f02beb73ca61e8a4d2
Fabrice Gabriel Calixte <f.gabrielcalixte@gmail.com>
2017-10-23 15:28:01 +0200

    modif du aboutus manager

--- src/Model/AboutUsManager.php
+++ src/Model/AboutUsManager.php
@@ -12,4 +12,22 @@ class AboutUsManager extends EntityManager
 
         return $statement->fetchAll(\PDO::FETCH_CLASS, AboutUs::class);
     }
+
+    /**
+     *   met a jours les nouveauté
+     */
+    public function upDateNews(AboutUs $aboutUs)
+    {
+        $query = "UPDATE about_us  
+                  (about_us, news_title, news, news_picture, mail, tel) 
+                  SET (:aboutus, :newstitle, :news, :newspicture, :mail, :tel)";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('about_us', $aboutUs->getAboutUs(), \PDO::PARAM_STR);
+        $statement->bindValue('newstitle', $aboutUs->getNewsTitle(), \PDO::PARAM_STR);
+        $statement->bindValue('newspicture', $aboutUs->getNewsPicture(), \PDO::PARAM_INT);
+        $statement->bindValue('news', $aboutUs->getNews(), \PDO::PARAM_INT);
+        $statement->bindValue('mail', $aboutUs->getMail(), \PDO::PARAM_INT);
+        $statement->bindValue('tel', $aboutUs->getTel(), \PDO::PARAM_INT);
+        $statement->execute();
+    }
 }
