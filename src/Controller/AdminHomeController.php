54e0c0808e9081d12fac254df65c1498bab8aa82
Fabrice Gabriel Calixte <f.gabrielcalixte@gmail.com>
2017-10-24 18:12:50 +0200

    creation de l'update des nouveauté

---
+++ src/Controller/AdminHomeController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Cataluna\Controller;
+
+use Cataluna\Model\AboutUs;
+use Cataluna\Model\AboutUsManager;
+
+class AdminHomeController extends Controller
+{
+    /**
+     * mise à jour des données du modèle
+     */
+    public function updateAction()
+    {
+// récupérer $_POST et traiter
+        $errors = [];
+// creation d'un objet aboutUs vide
+        $aboutUs = new AboutUs();
+        $aboutUsManager = new AboutUsManager();
+
+        if (!empty($_POST)) {
+// traitement des erreurs éventuelles
+            $aboutUs->setAboutUs($_POST['about_us']);
+            $aboutUs->setNewsTitle($_POST['news_title']);
+            $aboutUs->setNewsPicture($_POST['news_picture']);
+            $aboutUs->setNews($_POST['news']);
+            $aboutUs->setMail($_POST['mail']);
+            $aboutUs->setTel($_POST['tel']);
+
+
+            if (empty($_POST['about_us'])) {
+                $errors[] = 'Remplissez le champs Nos produits';
+            }
+
+            if (empty($_POST['news_title'])) {
+                $errors[] = 'Remplissez le champs Titre de la nouveauté';
+            }
+
+            if (empty($_POST['news_picture'])) {
+                $errors[] = 'Remplissez le champs Image de la nouveauté';
+            }
+
+            if (empty($_POST['news'])) {
+                $errors[] = 'Remplissez le champs Description de la nouveauté';
+            }
+
+            if (empty($_POST['mail'])) {
+                $errors[] = 'Remplissez le champs Mail';
+            }
+
+            if (empty($_POST['news'])) {
+                $errors[] = 'Remplissez le champs Téléphone';
+            }
+
+// si pas d'erreur, met à jour la bdd
+            if (empty($errors)) {
+                $aboutUsManager->updateAboutUs($aboutUs);
+                header('Location:index.php');
+            }
+        }
+
+        else {
+            $aboutUs= $aboutUsManager->findAll()[0];
+        }
+
+
+        return $this->twig->render('Home/updateHome.html.twig', [
+            'errors' => $errors,
+            'aboutUs' => $aboutUs,
+        ]);
+
+
+    }
+}
+
--- src/Model/AboutUsManager.php
+++ src/Model/AboutUsManager.php
@@ -16,18 +16,22 @@ class AboutUsManager extends EntityManager
     /**
      *   met a jours les nouveauté
      */
-    public function upDateNews(AboutUs $aboutUs)
+    public function updateAboutUs(AboutUs $aboutUs)
     {
-        $query = "UPDATE about_us  
-                  (about_us, news_title, news, news_picture, mail, tel) 
-                  SET (:aboutus, :newstitle, :news, :newspicture, :mail, :tel)";
+        $query = "UPDATE about_us   
+                  SET about_us = :aboutus,
+                      news_title = :newstitle,
+                      news = :news,
+                      news_picture = :newspicture,
+                      mail = :mail,
+                      tel = :tel";
         $statement = $this->pdo->prepare($query);
-        $statement->bindValue('about_us', $aboutUs->getAboutUs(), \PDO::PARAM_STR);
+        $statement->bindValue('aboutus', $aboutUs->getAboutUs(), \PDO::PARAM_STR);
         $statement->bindValue('newstitle', $aboutUs->getNewsTitle(), \PDO::PARAM_STR);
-        $statement->bindValue('newspicture', $aboutUs->getNewsPicture(), \PDO::PARAM_INT);
-        $statement->bindValue('news', $aboutUs->getNews(), \PDO::PARAM_INT);
-        $statement->bindValue('mail', $aboutUs->getMail(), \PDO::PARAM_INT);
-        $statement->bindValue('tel', $aboutUs->getTel(), \PDO::PARAM_INT);
+        $statement->bindValue('newspicture', $aboutUs->getNewsPicture(), \PDO::PARAM_STR);
+        $statement->bindValue('news', $aboutUs->getNews(), \PDO::PARAM_STR);
+        $statement->bindValue('mail', $aboutUs->getMail(), \PDO::PARAM_STR);
+        $statement->bindValue('tel', $aboutUs->getTel(), \PDO::PARAM_STR);
         $statement->execute();
     }
 }
